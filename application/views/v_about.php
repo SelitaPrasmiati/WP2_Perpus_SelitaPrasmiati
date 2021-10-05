b9c129dd84bd3d8ba6d73207e9445f88f5d29d8a
selitap <selitaprasmiati0620@gmail.com>
2021-10-05 17:33:10 +0700

    two web

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Selita </li>
+        <li>Nama Belakang : Prasmiati</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Cipinang Muara, Jakarta Timur</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kebumen</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Singing</li>
+        <li>Lari</li>
+    </ul>
+</section>
